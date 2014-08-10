c5892f7b637019c149df06963c3c4798d3200337
CoreOS Admin <twistedogic@gmail.com>
2014-08-10 11:15:39 +0000

    update functions

--- test.js
+++ test.js
@@ -2,24 +2,26 @@ var express = require('express');
 var fs = require('fs');
 var request = require('request');
 var app     = express();
-var winston = require('winston');
+var Mailgun = require('mailgun-js');
 
+//log
+// var winston = require('winston');
+// require('winston-logstash');
+// winston.add(winston.transports.Logstash, {
+//   port: 28777,
+//   node_name: 'my node name',
+//   host: '172.17.0.4'
+// });
+// winston.add(winston.transports.File, {
+//     filename: 'all-logs.log',
+//     handleExceptions: true
+// });
 // ENV config
 var port = process.env.PORT || '3000';
-require('winston-logstash');
 var api_key = 'key-3z2nwyjiq240lyqp6sz3t3a852toy9i2';
 var domain = 'twistedogic.mailgun.org';
-var Mailgun = require('mailgun-js');
 var mailgun = new Mailgun({apiKey: api_key, domain: domain});
-winston.add(winston.transports.Logstash, {
-  port: 28777,
-  node_name: 'my node name',
-  host: '172.17.0.4'
-});
-winston.add(winston.transports.File, {
-    filename: 'all-logs.log',
-    handleExceptions: true
-});
+
   
 // parameter
 var baseUrl = 'http://finance.yahoo.com/d/quotes.csv?';
@@ -30,14 +32,11 @@ var stockList = [
 	high:9.95
 },{
 	id:"0008.HK",
-	low:4.83,
+	low:4.93,
 	high:4.96
 }];
-var jsonSchema = {
-	id:"",
-	data:{}
-};
-var sampleRate = 1000;
+var jsonSchema = ["id","name","price","volume","high","low","ER","date"];
+var sampleRate = 5000;
 var timer = 0;
 var lowSent = new Array(stockList.length+1).join('0').split('').map(parseFloat);
 var highSent = lowSent;
@@ -49,21 +48,15 @@ function stockJSON(csv,schema,stockId){
 	var result = [];
 	for(var i=0;i<lines.length;i++){
 		var info = lines[i].replace(/\r|['"]+/g, '').split(",");
-		var obj = schema;
+		info.push(new Date());
+		var obj = {};
 		if (info[0] === stockId[i].id) {
-			obj.id = info[0];
-			obj.data.name = info[1];
-			obj.data.price = info[2];
-			obj.data.volume = info[3];
-			obj.data.high = info[4];
-			obj.data.low = info[5];
-			obj.data.ER = info[6];
-			obj.data.date = new Date();
+			for (var j = 0; j < schema.length; j++){
+				obj[schema[j]] = info[j];
+			}
 			result.push(obj);
-			console.log(obj);
 		}
 	}
-	console.log(result);
 	//return result; //JavaScript object
 	return result; //JSON
 }
@@ -76,24 +69,22 @@ function stockUrl(stockId){
 	return urlString;
 }
 
-function thresholdAlert(stockId,stockData){
-	console.log(stockData.data.price +","+ stockId.low);
-	if (stockData.data.price < stockId.low){
-		var message = {
+function thresholdAlert(stockData,stockId){
+	var message = null;
+	if (stockData.price < stockId.low){
+		message = {
 			from: 'Automated Alarm <me@twistedogic.mailgun.org>',
 			to: 'cloudogic@gmail.com',
-			subject: stockData.data.name + ' ' + stockData.id + ' has FALL' ,
-			text: '<h2 style="background-color:red;">' + stockData.data.price + '</h1>'
+			subject: stockData.name + ' ' + stockData.id + ' has FALL' ,
+			html: '<h2 style="background-color:red;">' + stockData.price + '</h1>'
 		};
-	} else if (stockData.data.price > stockId.high){
-		var message = {
+	} else if (stockData.price > stockId.high){
+		message = {
 			from: 'Automated Alarm <me@twistedogic.mailgun.org>',
 			to: 'cloudogic@gmail.com',
-			subject: stockData.data.name + ' ' + stockData.id + ' has RISE' ,
-			text: '<h2 style="background-color:green;">' + stockData.data.price + '</h1>'
+			subject: stockData.name + ' ' + stockData.id + ' has RISE' ,
+			html: '<h2 style="background-color:green;">' + stockData.price + '</h1>'
 		};
-	} else {
-		var message = null;
 	}
 	return message;
 }
@@ -103,31 +94,31 @@ function reportAlert(stockData){
 		from: 'Automated Alarm <me@twistedogic.mailgun.org>',
 		to: 'cloudogic@gmail.com',
 		subject: 'Daily report',
-		text: '<h2 style="background-color:red;">' + stockData.data.price + '</h1>'
+		text: '<h2 style="background-color:red;">' + stockData.price + '</h1>'
 	};
 	return data;
 }
 
 function emailAlert(data){
+	var sent = 0;
 	if(data){
-		mailgun.messages().send(data, function (error, body) {
-			console.log(body);
-		});
+		mailgun.messages().send(data, function (error, body) {});
 		sent = 1;
-	} else {
-		sent = 0;
+		console.log(data);
 	}
 	return sent;
 }
 // long poll
 setInterval(function(){
+	console.log(timer/1000);
 	timer = timer + sampleRate;
-	if (timer > 3600000){
+	if (timer > 10000){
 		timer = 0;
 		lowSent = new Array(stockList.length+1).join('0').split('').map(parseFloat);
 		highSent = lowSent;
+		console.log("reset");
 	}
-	list = stockUrl(stockList);
+	var list = stockUrl(stockList);
 	console.log(list);
 	url = baseUrl + 's=' + list + '&f=snl1vhgr';
 	request(url, function(error, response, html){
@@ -135,12 +126,18 @@ setInterval(function(){
 			var stock = stockJSON(html,jsonSchema,stockList);
 			for (var i = 0; i < stockList.length; i++){
 				if (highSent[i] <= 0){
-					highSent[i] = emailAlert(thresholdAlert(stock[i],stockList[i]));
+					// emailAlert(thresholdAlert(stock[i],stockList[i]));
+					highSent[i] = 1;
+					console.log(highSent[i]);
 					lowSent[i] = lowSent[i] - highSent[i];
+					console.log(lowSent[i]);
 				} else if (lowSent[i] <= 0){
-					lowSent[i] = emailAlert(thresholdAlert(stock[i],stockList[i]));
+					lowSent[i] = 1;
+					console.log(lowSent);
+					// emailAlert(thresholdAlert(stock[i],stockList[i]));
 					highSent[i] = highSent[i] - lowSent[i];
 				}
+				console.log(highSent[i]);
 			}
 		}
 	});
